b48b30614c38a1578815bd6d1fc356a4af7d44eb
unknown <keysalena25@gmail.com>
2026-05-21 18:10:06 +0700

    tambah workflow CI

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import App from './App'
 describe('App', () => {
   it('menampilkan nama profil', () => {
     render(<App />)
-    expect(screen.getByText('Keysa/i')).toBeInTheDocument()
+    expect(screen.getByText('/Keysa/i')).toBeInTheDocument()
 })
   it('menampilkan github link', () => {
     render(<App />)
